70281913ad89416386f1fdc7b461734861fb0cb7
Cesar Orellana <cesarlorellana184@gmail.com>
2025-06-08 21:00:10 -0300

    Creacion-de-lista-de-tarea

---
+++ ToDo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tarea;
+List<Tareas> tareasPendientes = new List<Tareas>();
+List<Tareas> tareasRealizas = new List<Tareas>();
+
+int Cantidad;
+string? Scantidad;
+Console.WriteLine("Ingrese la cantidad de Tareas:");
+Scantidad = Console.ReadLine();
+bool resultCant = int.TryParse(Scantidad, out Cantidad);
+int ID = 100, tiempo = 0;
+string? Stiempo;
+string? Descripcion;
+for (int i = 0; i < Cantidad; i++)
+{
+    Console.WriteLine("Ingrese la descripcion de su Tarea:");
+    Descripcion = Console.ReadLine();
+    Console.WriteLine("Ingrese la duracion de su Tarea:");
+    Stiempo = Console.ReadLine();
+    bool resultTime = int.TryParse(Stiempo, out tiempo);
+    tareasPendientes[i] = new Tareas(ID, Descripcion, tiempo);
+    ID++;
+}
+Console.WriteLine("Lista de Tareas Pendientes");
+foreach (var lista in tareasPendientes)
+{
+    Console.WriteLine($"Tarea: {lista.Tareaid}");
+    Console.WriteLine($"Descripcion:{lista.Descripcion}");
+    Console.WriteLine($"Duracion:{lista.Duracion}");
+    Console.WriteLine("---------");
+}
---
+++ ToDo/Tareas.cs
@@ -0,0 +1,20 @@
+namespace Tarea
+{
+    public class Tareas
+    {
+        private int tareaid;
+        private string? descripcion;
+        private int duracion;
+
+        public int Tareaid { get => tareaid; set => tareaid = value; }
+        public string? Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+
+        public Tareas(int tareaid, string? descripcion, int duracion)
+        {
+            this.Tareaid = tareaid;
+            this.Duracion = duracion;
+            this.Descripcion = descripcion;
+        }
+    }
+}

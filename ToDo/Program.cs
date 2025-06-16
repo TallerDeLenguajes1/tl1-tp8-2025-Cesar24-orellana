1f140f47136855652331106d0115357bd3445935
Cesar Orellana <cesarlorellana184@gmail.com>
2025-06-16 17:46:44 -0300

    Mejora-de-funcion-mover-tarea

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Tarea;
-var tareasPendientes = new List<Tareas>( );
+var tareasPendientes = new List<Tareas>();
 var tareasRealizas = new List<Tareas>();
 
 int Cantidad;
@@ -16,62 +16,85 @@ MostrarLista(tareasPendientes);
 MoverTareas(tareasPendientes, tareasRealizas);
 Console.WriteLine("Lista de Tareas Realizadas");
 MostrarLista(tareasRealizas);
+Console.WriteLine("---------");
+Console.WriteLine("Lista de Tareas Pendientes");
+MostrarLista(tareasPendientes);
 
 void crearTareas(List<Tareas> ListaTarea, int Cantidad)
 {
-int tiempo = 0;
-string? Stiempo = "";
-string? Descripcion;
-for (int i = 0; i < Cantidad; i++)
-{
-    Console.WriteLine("Ingrese la duracion de su Tarea, (10 a 100)min:");
-    Stiempo = Console.ReadLine();
-    bool resultTime = int.TryParse(Stiempo, out tiempo);
-    while (tiempo < 10 || tiempo > 100)
+    int tiempo = 0;
+    string? Stiempo = "";
+    string? Descripcion;
+    for (int i = 0; i < Cantidad; i++)
     {
-        Console.WriteLine(@"!! El tiempo tiene que estar definido entre 10 a 100 min");
-        Console.WriteLine("Ingrese una nueva duracion entre 10 a 100 min");
+        Console.WriteLine("Ingrese la duracion de su Tarea, (10 a 100)min:");
         Stiempo = Console.ReadLine();
-        bool resultTime2 = int.TryParse(Stiempo, out tiempo);
+        bool resultTime = int.TryParse(Stiempo, out tiempo);
+        while (tiempo < 10 || tiempo > 100)
+        {
+            Console.WriteLine(@"!! El tiempo tiene que estar definido entre 10 a 100 min");
+            Console.WriteLine("Ingrese una nueva duracion entre 10 a 100 min");
+            Stiempo = Console.ReadLine();
+            bool resultTime2 = int.TryParse(Stiempo, out tiempo);
+        }
+
+        Console.WriteLine($"Ingrese la descripcion de su Tarea{i + 1}:");
+        Descripcion = Console.ReadLine();
+
+
+        var tareaAux = new Tareas(i + 1, Descripcion, tiempo);
+        ListaTarea.Add(tareaAux);
     }
-
-    Console.WriteLine($"Ingrese la descripcion de su Tarea{i+1}:");
-    Descripcion = Console.ReadLine();
-
-
-    var tareaAux = new Tareas(i+1,Descripcion,tiempo);
-    ListaTarea.Add(tareaAux);
-}
 }
 
-void MostrarLista(List<Tareas> ListasTarea){
+void MostrarLista(List<Tareas> ListasTarea)
+{
     foreach (var lista in ListasTarea)
     {
-    Console.WriteLine($"Tarea: {lista.Tareaid}");
-    Console.WriteLine($"Descripcion:{lista.Descripcion}");
-    Console.WriteLine($"Duracion:{lista.Duracion}");
-    Console.WriteLine("---------");
+        Console.WriteLine($"Tarea: {lista.Tareaid}");
+        Console.WriteLine($"Descripcion:{lista.Descripcion}");
+        Console.WriteLine($"Duracion:{lista.Duracion}");
+        Console.WriteLine("---------");
     }
 }
 
-void MoverTareas(List<Tareas> pendiente, List<Tareas> realizada){
-int ID=0;
-string? Sid;
-Console.WriteLine("Escriba el ID que desea marcar como Completada");
-Sid = Console.ReadLine();
-bool resultID = int.TryParse(Sid, out ID);
-int Cantidad = pendiente.Count();
-for (int i = 0; i < Cantidad; i++)
+void MoverTareas(List<Tareas> pendiente, List<Tareas> realizada)
 {
-    if (pendiente[i].Tareaid == ID)
+    int ID = 0;
+    //string? Sid;
+    Console.WriteLine("Escriba el ID que desea marcar como Completada");
+    /* Sid = Console.ReadLine();
+    bool resultID = int.TryParse(Sid, out ID); */
+    if (int.TryParse(Console.ReadLine(), out ID))
     {
-        var tareaMover = new Tareas(pendiente[i].Tareaid, pendiente[i].Descripcion, pendiente[i].Duracion);
-
-    if (tareaMover != null)
-    {
-        realizada.Add(tareaMover);
-        pendiente.Remove(tareaMover);
-    } 
+        var tareaMover = pendiente.Find(t => t.Tareaid == ID);
+        if (tareaMover != null)
+        {
+            realizada.Add(tareaMover);
+            pendiente.Remove(tareaMover);
+            Console.WriteLine("Tarea agregada a realizadas");
+            Console.WriteLine($"Tarea: {tareaMover.Tareaid}");
+            Console.WriteLine($"Descripcion:{tareaMover.Descripcion}");
+            Console.WriteLine($"Duracion:{tareaMover.Duracion}");
+            Console.WriteLine("---------");
+        }
+        else
+        {
+            Console.WriteLine("No se encontro su tarea");
+        }
     }
-}
+    /* int Cantidad = pendiente.Count();
+for (int i = 0; i < Cantidad; i++)
+    {
+        if (pendiente[i].Tareaid == ID)
+        {
+            var tareaMover = new Tareas(pendiente[i].Tareaid, pendiente[i].Descripcion, pendiente[i].Duracion);
+
+            if (tareaMover != null)
+            {
+                realizada.Add(tareaMover);
+                pendiente.Remove(tareaMover);
+            }
+        }
+    } */
 }
--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07aa95b9eccfa9ebb3c656507371171a73aa3eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26d91945ee40c960b0fff27f8657a79e99a8f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

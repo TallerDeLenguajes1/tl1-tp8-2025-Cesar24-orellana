63beea61b9ab87a9a22ed1de3d90cdfa1b51b881
Cesar Orellana <cesarlorellana184@gmail.com>
2025-06-21 21:48:44 -0300

    Historial-Calculadora

--- CalculadoraHistorial/Operaciones.cs
+++ CalculadoraHistorial/Operaciones.cs
@@ -42,7 +42,7 @@ namespace CalculadoraHistorial
             };
             return operacion == TipoOperacion.limpiar
             ? "Se Limpio el Resultado"
-            : $"{resultadoAnterior}  {simbolo} {nuevoValor} = {Resultado}";
+            : $"{resultadoAnterior} {simbolo} {nuevoValor} = {Resultado}";
         }
         public enum TipoOperacion
         {
@@ -52,36 +52,5 @@ namespace CalculadoraHistorial
             Division,
             limpiar
         }
-        /* private double resultado;
-        public double Resultado
-        {
-            get => resultado;
-            set => resultado = value;
-        }
-        public double dato
-        {
-            get => resultado;
-            set => resultado = value;
-        }
-        public void Sumar(double termino)
-        {
-            resultado += termino;
-        }
-        public void Resta(double termino)
-        {
-            resultado -= termino;
-        }
-        public void Multiplicacion(double termino)
-        {
-            resultado *= termino;
-        }
-        public void Dividir(double termino)
-        {
-            resultado /= termino;
-        }
-        public void limpiar()
-        {
-            resultado = 0;
-        } */
     }
 }
